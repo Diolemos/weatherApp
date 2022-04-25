e1634c2071b9e5c8d404289063d03e7efa752843
Dio Lemos <pedrodiogenes@outlook.com>
2022-04-24 21:26:18 -0300

    App is functiona

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const APIkey = 'bd401b2a40e7bc2def1d1a1a4c4b37d8';
 let cityName = "London"
-
-
+let units = "metric"
+const unitCheckBox = document.querySelector("#metric")
 let weatherData = {};
 const btn = document.getElementById("search-btn");
 const textBox = document.getElementById("location-input")
@@ -9,11 +9,13 @@ const textBox = document.getElementById("location-input")
 const metricSwitch = document.getElementById("metric");
 
 // weather card items
-const currentCityName = document.querySelector(".cityname");
-const currentCountry = document.querySelector(".state");
-const currentTemperature = document.querySelector(".temperature")
-
-
+const CityNameNode = document.querySelector(".cityname");
+const country = document.querySelector(".country")
+const temperature = document.querySelector(".temp")
+const max = document.querySelector(".max")
+const min = document.querySelector(".min")
+const feelsLike = document.querySelector(".feels-like")
+const windSpeed = document.querySelector(".w-speed")
 
 
 getWeatherData()
@@ -27,18 +29,29 @@ btn.addEventListener("click", function(){
     
 })
 
-
+unitCheckBox.addEventListener("change", ()=>{
+    if(units == "metric"){
+        units = "imperial"
+    }else{
+        units = "metric";
+    }
+})
 
 
 async function getWeatherData(){
     console.log(cityName)
-    let Url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=metric`;
+    let Url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=${units}`;
     let response = await fetch( Url, {mode: "cors"})
       weatherData  = await response.json();
       await updateCurrentWeather()     
 }
 
 function updateCurrentWeather(){
-    currentCityName.textContent = weatherData.name;
-    currentTemperature.textContent = weatherData.main.temp;
+    CityNameNode.textContent = weatherData.name;
+    temperature.textContent = weatherData.main.temp;
+    country.textContent = weatherData.sys.country;
+    max.textContent = weatherData.main.temp_max
+    min.textContent = weatherData.main.temp_min
+    feelsLike.textContent = weatherData.main.feels_like
+    windSpeed.textContent = weatherData.wind.speed
 }
